6ea190653042489871a4fd77cfb67017db969c49
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-10-10 14:46:20 +0100

    Adding Doclick Project

---
+++ src/main/java/com/doclick/DoclickApplication.java
@@ -0,0 +1,13 @@
+package com.doclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoclickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoclickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doclick/DoclickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doclick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoclickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
